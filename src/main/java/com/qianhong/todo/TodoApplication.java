846bc84361d690da8eb7af11ae37abb698a84f83
maping <maping930883@hotmail.com>
2018-09-06 00:18:21 +0800

    update pom.xml

--- src/main/java/com/qianhong/todo/TodoApplication.java
+++ src/main/java/com/qianhong/todo/TodoApplication.java
@@ -7,21 +7,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-
 public class TodoApplication extends SpringBootServletInitializer {
 
-	private static final Logger log = LoggerFactory.getLogger(TodoApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(TodoApplication.class);
 
-	@Override
+    @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(TodoApplication.class);
     }
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 }
